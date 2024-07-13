300ef9a1f11e86ef01f374bc76e3d31d10728cfb
Tùng Lâm <lambmt.work@gmail.com>
2024-07-14 02:34:30 +0700

    update

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -49,9 +49,9 @@ const EmailSection = () => {
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or just want to say hi, I'll try my best
-          to get back to you!
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
+          try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/lambmt2k">
