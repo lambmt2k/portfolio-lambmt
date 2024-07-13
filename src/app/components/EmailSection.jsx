ac3aef5c3dc1e97c363414312ede30c53ea9aca2
Tùng Lâm <lambmt.work@gmail.com>
2024-07-14 02:44:30 +0700

    fix bug

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,7 +47,6 @@ const EmailSection = () => {
           Let&apos;s Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
